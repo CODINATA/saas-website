08e3170098d1a78655c26a3c6aaed2ab59b697d1
adam <ademata13@gmail.com>
2025-05-07 14:16:24 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'saas-website',
   optimizeDeps: {
     exclude: ['lucide-react'],
+    
   },
 });
